5840f1ccc34b3c91ead8d3014b0ca5b33048bc1a
“Isabelle <118849336+isb05@users.noreply.github.com>
2025-04-08 19:05:57 -0400

    This is the about page and a home page.

--- home/views.py
+++ home/views.py
@@ -1,3 +1,13 @@
 from django.shortcuts import render
 
 # Create your views here.
+
+from django.shortcuts import render
+def index(request):
+    template_data = {}
+    template_data['title'] = 'FitJacket'
+    return render(request, 'home/index.html', {'template_data': template_data})
+def about(request):
+    template_data = {}
+    template_data['title'] = 'About'
+    return render(request, 'home/about.html',{'template_data': template_data})
