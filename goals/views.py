9b11eef9ba824a5ebb62815b20f8eb7283a907e5
thomasg13 <thomasguo13@gmail.com>
2025-04-07 22:12:10 -0400

    basic goal logging

--- goals/views.py
+++ goals/views.py
@@ -1,5 +1,21 @@
-from django.shortcuts import render
+from django.contrib.auth.decorators import login_required
+from django.shortcuts import render, redirect
+
+from goals.forms import FitnessGoalForm
+from goals.models import FitnessGoal
+
 
 # Create your views here.
+@login_required
 def goals_home(request):
-    return render(request, 'goals/goals_home.html')
+    goal, created = FitnessGoal.objects.get_or_create(user=request.user)
+
+    if request.method == 'POST':
+        form = FitnessGoalForm(request.POST, instance=goal)
+        if form.is_valid():
+            form.save()
+            return redirect('goals:goals_home')
+    else:
+        form = FitnessGoalForm(instance=goal)
+
+    return render(request, 'goals/goals_home.html', {'form': form, 'goal': goal})
