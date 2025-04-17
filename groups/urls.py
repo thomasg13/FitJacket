a17c3023c111956059be036fc5a9600585061b08
isb05 <isabellewebb05@gmail.com>
2025-04-17 13:49:56 -0400

    added ability to join and leave workout groups

---
+++ groups/urls.py
@@ -0,0 +1,10 @@
+from django.urls import path
+from . import views
+
+app_name = 'groups'
+urlpatterns = [
+    path('', views.workout_groups, name='workout_groups_list'),
+    path('join/<int:group_id>/', views.join_group, name='join_group'),
+    path('leave/<int:group_id>/', views.leave_group, name='leave_group'),
+    path('groups/', views.workout_groups_view, name='workout_groups_list')
+]
