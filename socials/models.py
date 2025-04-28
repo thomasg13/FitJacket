763cea0ff6e1d5058fb55354d62ffd00f7213649
thomasg13 <thomasguo13@gmail.com>
2025-04-28 00:41:06 -0400

    friend requests and friends list

--- socials/models.py
+++ socials/models.py
@@ -1,3 +1,16 @@
 from django.db import models
+from django.conf import settings
 
 # Create your models here.
+
+class FriendRequest(models.Model):
+    from_user = models.ForeignKey(settings.AUTH_USER_MODEL, related_name='sent_requests', on_delete=models.CASCADE)
+    to_user = models.ForeignKey(settings.AUTH_USER_MODEL, related_name='received_requests', on_delete=models.CASCADE)
+    is_accepted = models.BooleanField(default=False)
+    timestamp = models.DateTimeField(auto_now_add=True)
+
+    class Meta:
+        unique_together = ('from_user', 'to_user')  # prevent duplicate requests
+
+    def __str__(self):
+        return f"{self.from_user} ➔ {self.to_user}"
--- socials/urls.py
+++ socials/urls.py
@@ -6,4 +6,6 @@ app_name = 'socials'
 urlpatterns = [
     path("", views.home, name='home'),
     path("search/", views.search_users, name="search_users"),
+    path("friend-request/send/<int:user_id>/", views.send_friend_request, name="send_friend_request"),
+    path("friend-request/accept/<int:request_id>/", views.accept_friend_request, name="accept_friend_request"),
 ]
