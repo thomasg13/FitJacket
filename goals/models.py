ea2619192446458683e59596ae13410ec5266d3d
thomasg13 <thomasguo13@gmail.com>
2025-04-28 23:33:47 -0400

    change default fitness goals

--- goals/models.py
+++ goals/models.py
@@ -4,7 +4,7 @@ from django.contrib.auth.models import User
 # Create your models here.
 class FitnessGoal(models.Model):
     user = models.OneToOneField(User, on_delete=models.CASCADE)
-    weekly_miles_goal = models.DecimalField(max_digits=5, decimal_places=1, default=0)
-    weekly_workout_days_goal = models.IntegerField(default=0)
-    daily_steps = models.PositiveIntegerField(default=0)
-    daily_calories = models.PositiveIntegerField(default=0)
+    weekly_miles_goal = models.DecimalField(max_digits=5, decimal_places=1, default=10)
+    weekly_workout_days_goal = models.IntegerField(default=4)
+    daily_steps = models.PositiveIntegerField(default=10000)
+    daily_calories = models.PositiveIntegerField(default=2000)
